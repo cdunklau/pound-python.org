c0098c7de60b5b12d093a51acea8c24aa8085019
Colin Dunklau <colin.dunklau@gmail.com>
2018-09-24 23:04:48 +0200

    Update to support github pages site

--- README.rst
+++ README.rst
@@ -7,11 +7,16 @@ Virtualenv + deps::
     venv/bin/pip install --upgrade pip setuptools
     venv/bin/pip install docutils black
 
+Clone destination::
+
+    git clone git@github.com:pound-python/pound-python.github.io.git
+
 Build::
 
     venv/bin/python3 build.py
 
-Output HTML will be in the output/ dir.
+Output HTML will be in the pound-python.github.io/ repo. Commit and push
+in that repo to publish.
 
 
 Use `Black <https://github.com/ambv/black>`_ to format the build script.
--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import urllib.parse
 
 HERE = pathlib.Path(__file__).parent
 SOURCES_DIR = HERE.joinpath("sources")
-OUTPUT_DIR = HERE.joinpath("output")
+OUTPUT_DIR = HERE.joinpath("pound-python.github.io")
 
 # TODO: Maybe rework this to use the docutils API directly
 
@@ -34,16 +34,10 @@ def main():
     rel_rst = src_rst.relative_to(SOURCES_DIR)
     dest_html = OUTPUT_DIR.joinpath(rel_rst.parent, rel_rst.stem + ".html")
 
-    if OUTPUT_DIR.exists():
-        print("Cleaning {0}".format(OUTPUT_DIR))
-        shutil.rmtree(OUTPUT_DIR)
-        print("Creating {0}".format(OUTPUT_DIR))
-        OUTPUT_DIR.mkdir()
-
     print("Writing {src} to {dest}".format(src=src_rst, dest=dest_html))
     build_one(src_rst, dest_html, stylesheet_url)
     print("Copying stylesheet")
-    stylesheet_dest.parent.mkdir()
+    stylesheet_dest.parent.mkdir(exist_ok=True)
     shutil.copy2(stylesheet_src, stylesheet_dest)
 
 
